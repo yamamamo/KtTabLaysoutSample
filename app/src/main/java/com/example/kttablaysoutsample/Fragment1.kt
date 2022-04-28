6e07658573f64141f1408264d449942bd92608ce
yamamamo <yamamamamo@gmail.com>
2022-04-28 21:38:39 +0900

    FragmentAdapter

--- app/src/main/java/com/example/kttablaysoutsample/Fragment1.kt
+++ app/src/main/java/com/example/kttablaysoutsample/Fragment1.kt
@@ -5,21 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.kttablaysoutsample.databinding.Fragment1Binding
 
 class Fragment1 : Fragment() {
 
+    private lateinit var binding: Fragment1Binding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false)
+        binding = Fragment1Binding.inflate(inflater,container,false)
+        return binding.root
+//        return inflater.inflate(R.layout.fragment_1, container, false)
     }
 
-    private fun newInstant() : Fragment1 {
-        val args = Bundle()
-        val frag = Fragment1()
-        frag.arguments = args
-        return frag
-    }
 }
